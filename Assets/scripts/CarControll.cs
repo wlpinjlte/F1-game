80645919ab8a895856b8c6f4a00e43d8257f9b99
Mateusz Waga <wagulec123@gmail.com>
2025-04-07 21:44:14 +0200

    improve car controll and add new track

--- Assets/scripts/CarControll.cs
+++ Assets/scripts/CarControll.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
 
-public class CarControll
+public class CarControll1 : MonoBehaviour
 {
-    
+    public WheelCollider frontLeftWheel;
+    public WheelCollider frontRightWheel;
+    public WheelCollider rearLeftWheel;
+    public WheelCollider rearRightWheel;
+
+    public float motorTorque = 10000f; // Sta�a moc silnika
+    public float maxSteeringAngle = 25f; // Maksymalny k�t skr�tu
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 1000000f;
+        rb.linearDamping = 0.1f; // lub nawet mniejsze
+        rb.angularDamping = 0.05f;
+    }
+
+    void FixedUpdate()
+    {
+        MoveForward();
+        HandleSteering();
+    }
+
+    void MoveForward()
+    {
+        float Vdirection = Input.GetAxis("Vertical");
+        frontLeftWheel.motorTorque = motorTorque * Vdirection;
+        frontRightWheel.motorTorque = motorTorque * Vdirection;
+    }
+
+    void HandleSteering()
+    {
+        float steering = Input.GetAxis("Horizontal") * maxSteeringAngle;
+        frontLeftWheel.steerAngle = steering;
+        frontRightWheel.steerAngle = steering;
+    }
 }
